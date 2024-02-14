b12ad3dc1a2745a105320d6e08471e82b9752d38
Teodor168 <teodor16iliev@gmail.com>
2024-02-14 09:44:54 +0200

    proba za purvi put

---
+++ Ocenki/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ocenki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklariram masiv ot 13 elementa
+            double[] grades = new double[13];//{1,2,3,4,5,6};
+            string[] names = new string[13] {"Lubo", "Milen", "Misho",
+           "Rado", "Svetlo", "Siana", "Silviq"
+            ,"Staskata", "Az", "Hristiqn", "Hristomir",
+           "Cvetelina", "Cvetina"};
+            //Vavejdane na elementite ot klaviaturata-cikul
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write($"{i+14}. {names[i]} e ==>");
+                grades[i] = double.Parse(Console.ReadLine());
+            }
+           
+            //Obrabotka
+            grades[8] = 6;
+            names[8] = "Teodor";
+            //Otpechatvane na rezultatite
+
+            for(int j = 0;j < names.Length;j++)
+            {
+                Console.WriteLine($"na {j+14}. {grades[j]}");
+            }
+
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Da se vuvede n za broqt na elementite v masiv
+            //Da se deklarira masiv ot celi chisla
+            //Da se vuvedat vsichki elementi na masiva
+            //Da se otpechati sumata ot vsichki elementi
+            //Srednoto aretmeticno na elementite
+            //Nai golemiqt element ot masiva i na koe mqsto e
+            Console.WriteLine("Dai chislo");
+            int num = int.Parse(Console.ReadLine());
+            int[] array = new int[num];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($" array[{i}] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //sumata
+            long sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine($"Sum={sum}");
+
+            //min
+            int min = int.MaxValue;
+            int minPos = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                min = array[i];
+                minPos = i;
+            }
+            Console.WriteLine($"{min}===>{minPos}");
+
+
+            // qko print
+            for (int i = 0; i < array.Length ; i++)
+            {
+                Console.WriteLine($" array[{i}] = {array[i]}");
+
+            }
+
+        }
+    }
+}
